a8a07efe56f92254ef9aebb264bf21f304313658
Paxnamra <bialyfeniks4@gmail.com>
2020-08-09 23:54:09 +0200

    number guesser: added guess comparision function

--- number_guesser/script.js
+++ number_guesser/script.js
@@ -5,5 +5,13 @@ let currentRoundNumber = 1;
 // Write your code below:
 function generateTarget() {
     return Math.floor(Math.random() * 9);
-   };
+};
+
+function compareGuesses(humanGuess, computerGuess, number) {
+    if (humanGuess !== computerGuess) {
+        return ((number - humanGuess) > (number - computerGuess)) ? true : false;
+    } else {
+        return true;
+    }
+};
 
