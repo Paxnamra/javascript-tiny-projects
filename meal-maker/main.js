198ada839fbbe8fadd0859548e127f3acfb2d5ad
Paxnamra <bialyfeniks4@gmail.com>
2020-08-13 23:54:28 +0200

    meal maker: added function to add new dish to the course

--- meal-maker/main.js
+++ meal-maker/main.js
@@ -1,36 +1,46 @@
 let menu = {
   _courses: {
-      appetizers: [],
-      mains: [],
-      desserts: [],
+    appetizers: [],
+    mains: [],
+    desserts: [],
   },
   get appetizers() {
-      return this._courses['appetizers'];
+    return this._courses['appetizers'];
   },
   get mains() {
-      return this._courses['mains'];
+    return this._courses['mains'];
   },
   get desserts() {
-      return this._courses['desserts'];
+    return this._courses['desserts'];
   },
   set appetizers(appetizers) {
-      this._courses['appetizers'] = appetizers;
+    this._courses['appetizers'] = appetizers;
   },
   set mains(mains) {
-      this._courses['mains'] = mains;
+    this._courses['mains'] = mains;
   },
   set desserts(desserts) {
-      this._courses['desserts'] = desserts;
+    this._courses['desserts'] = desserts;
   },
   get courses() {
-      return {
-          appetizers: this.appetizers,
-          mains: this.mains,
-          desserts: this.desserts
-      };
-  }
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts
+    };
+  },
+  addDishToCourse(courseName, dishName, dishPrice) {
+    const dish = {
+      dishName: dishName,
+      dishPrice: dishPrice,
+    };
+    return this._courses[courseName].push(dish);
+  },
 }
 
 console.log(menu.appetizers);
 console.log(menu.courses);
 console.log(menu._courses.courses);
+
+menu.addDishToCourse('appetizers', 'olives', 3.25);
+console.log(menu.appetizers);
