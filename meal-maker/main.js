60ad6e3cf07cae00dbb291b3fd5c47381f9582e1
Paxnamra <bialyfeniks4@gmail.com>
2020-08-13 23:48:25 +0200

    meal maker: getters and setters for menu object accessing nested properties

--- meal-maker/main.js
+++ meal-maker/main.js
@@ -1,10 +1,36 @@
 let menu = {
-    _courses: {
+  _courses: {
       appetizers: [],
       mains: [],
       desserts: [],
-    },
-    
-};
+  },
+  get appetizers() {
+      return this._courses['appetizers'];
+  },
+  get mains() {
+      return this._courses['mains'];
+  },
+  get desserts() {
+      return this._courses['desserts'];
+  },
+  set appetizers(appetizers) {
+      this._courses['appetizers'] = appetizers;
+  },
+  set mains(mains) {
+      this._courses['mains'] = mains;
+  },
+  set desserts(desserts) {
+      this._courses['desserts'] = desserts;
+  },
+  get courses() {
+      return {
+          appetizers: this.appetizers,
+          mains: this.mains,
+          desserts: this.desserts
+      };
+  }
+}
 
-    console.log(menu._courses);
+console.log(menu.appetizers);
+console.log(menu.courses);
+console.log(menu._courses.courses);
