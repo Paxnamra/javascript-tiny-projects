acb54ce69ed19632b6727b52c4d04b9dec76ff9b
Paxnamra <bialyfeniks4@gmail.com>
2020-08-15 23:48:28 +0200

    team stats: populated arrays with data

--- team-stats/main.js
+++ team-stats/main.js
@@ -1,5 +1,40 @@
 let team = {
-    _players: [],
-    _games: []
+    _players: [
+        {
+            firstName: 'Ash',
+            lastName: 'Ketchum',
+            age: 15
+        },
+        {
+            firstName: 'Teddy',
+            lastName: 'TheBear',
+            age: 10
+        },
+        {
+            firstName: 'Sal',
+            lastName: 'Firestarter',
+            age: 20
+        }
+    ],
+    _games: [
+        {
+            opponent: 'Pegasus',
+            teamPoints: 30,
+            opponentPoints: 15
+        },
+        {
+            opponent: 'Sacramento',
+            teamPoints: 19,
+            opponentPoints: 23
+        },
+        {
+            opponent: 'Thor',
+            teamPoints: 42,
+            opponentPoints: 39
+        },
+    ],
 };
 
+console.log(team._players);
+console.log();
+console.log(team._games);
