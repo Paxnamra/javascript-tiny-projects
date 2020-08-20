298f3adc7539af079fcdc2c034e77b183b69873e
Paxnamra <bialyfeniks4@gmail.com>
2020-08-21 01:21:01 +0200

    library: code formatting

--- library/main.js
+++ library/main.js
@@ -1,74 +1,74 @@
 class Media {
     constructor(param) {
-      this._title = param;
-      this._isCheckedOut = false;
-      this._ratings = [];
+        this._title = param;
+        this._isCheckedOut = false;
+        this._ratings = [];
     }
-  
+
     get title() {
-      return this._title;
+        return this._title;
     }
-  
+
     get isCheckedOut() {
-      return this._isCheckedOut;
+        return this._isCheckedOut;
     }
-  
+
     get ratings() {
-      return this._ratings;
+        return this._ratings;
     }
-  
+
     set isCheckedOut(checkedOut) {
-      this._isCheckedOut = checkedOut;
+        this._isCheckedOut = checkedOut;
     }
-  
+
     toggleCheckOutStatus() {
-      this._isCheckedOut = !this._isCheckedOut;
+        this._isCheckedOut = !this._isCheckedOut;
     }
-  
+
     addRating(input) {
-      this.ratings.push(input);
+        this.ratings.push(input);
     }
-  
+
     getAverageRating() {
-      const reducer = (a, b) => a + b;
-      const arrValue = this._ratings.length
-      return (this.ratings.reduce(reducer)) / arrValue;
+        const reducer = (a, b) => a + b;
+        const arrValue = this._ratings.length
+        return (this.ratings.reduce(reducer)) / arrValue;
     }
-  }
+}
 
-  class Book extends Media {
+class Book extends Media {
     constructor(author, title, pages) {
-      super(title);
-      this._author = author;
-      this._pages = pages;
+        super(title);
+        this._author = author;
+        this._pages = pages;
     }
-  
+
     get author() {
-      return this._author;
+        return this._author;
     }
-  
+
     get pages() {
-      return this._pages;
+        return this._pages;
     }
-  }
+}
 
-  class Movie extends Media {
+class Movie extends Media {
     constructor(title, director, runTime) {
-      super(title);
-      this._director = director;
-      this._runTime = runTime;
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
     }
-  
-    get director() {
-      return this._director;
-    }
-  
-    get runTime() {
-      return this._runTime;
-    }
-  }
 
-  const historyOfEverything = new Book('Bill Bryson','A Short History of Nearly Everything', 544);
+    get director() {
+        return this._director;
+    }
+
+    get runTime() {
+        return this._runTime;
+    }
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
 
 historyOfEverything.toggleCheckOutStatus();
 console.log(historyOfEverything.isCheckedOut);
@@ -81,7 +81,7 @@ const avRat = historyOfEverything.getAverageRating();
 console.log(avRat);
 console.log();
 
-const speed = new Movie('Speed','Jan de Bont', 116);
+const speed = new Movie('Speed', 'Jan de Bont', 116);
 speed.toggleCheckOutStatus();
 console.log(speed.isCheckedOut);
 
