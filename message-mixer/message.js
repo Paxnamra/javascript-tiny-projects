60004a57fae4c0b99ccf4109eca38e422319654f
Paxnamra <bialyfeniks4@gmail.com>
2020-08-27 00:22:48 +0200

    message mixer: changed require() to import from in message.js

--- message-mixer/message.js
+++ message-mixer/message.js
@@ -1,4 +1,4 @@
-const MessageMixer = require('./messageMixer.js');
+import MessageMixer from './messageMixer';
 
 function displayMessage() {
     console.log(MessageMixer.countCharacter("What is the color of the sky?", "t"));
