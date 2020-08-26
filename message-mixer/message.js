f57991ece20e87a802b91673b42ce5a5c179e7f9
Paxnamra <bialyfeniks4@gmail.com>
2020-08-27 01:39:33 +0200

    message mixer: switched the way of functions imports in message.js to named variables import || console loged pigLatin and palindrome functions

--- message-mixer/message.js
+++ message-mixer/message.js
@@ -1,12 +1,14 @@
-import MessageMixer from './messageMixer';
+import { countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin } from './messageMixer';
 
 function displayMessage() {
-    console.log(MessageMixer.countCharacter("What is the color of the sky?", "t"));
-    console.log(MessageMixer.capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-    console.log(MessageMixer.reverseWord("What is the color of the sky?"));
-    console.log(MessageMixer.reverseAllWords("What is the color of the sky?"));
-    console.log(MessageMixer.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-    console.log(MessageMixer.encode("What is the color of the sky?"));
+    console.log(countCharacter("What is the color of the sky?", "t"));
+    console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+    console.log(reverseWord("What is the color of the sky?"));
+    console.log(reverseAllWords("What is the color of the sky?"));
+    console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+    console.log(encode("What is the color of the sky?"));
+    console.log(palindrome('1eee 2a 3ff 4bbb'));
+    console.log(pigLatin('1eee 2a 3ff 4bbb', 'RRR'));
 }
 
 displayMessage();
