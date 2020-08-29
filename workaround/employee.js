20891f1f85eb801651e0eb732049c4b54e080d90
Paxnamra <bialyfeniks4@gmail.com>
2020-08-29 19:47:44 +0200

    workaround: set up export employee.js by variable names and aliases

--- workaround/employee.js
+++ workaround/employee.js
@@ -3,9 +3,14 @@ let Employee = {
 };
 
 let payGrades = {
-    entryLevel: { taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999 },
-    midLevel: { taxMultiplier: .1, benefits: ['health', 'housing'], minSalary: 50000, maxSalary: 99999 },
-    seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
+    entryLevel: {taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999},
+    midLevel: {taxMultiplier: .1, benefits: ['health', 'housing'], minSalary: 50000, maxSalary: 99999},
+    seniorLevel: {
+        taxMultiplier: .2,
+        benefits: ['health', 'housing', 'wellness', 'gym'],
+        minSalary: 100000,
+        maxSalary: 200000
+    }
 };
 
 function getCadre() {
@@ -29,7 +34,7 @@ function calculateBonus() {
 }
 
 function reimbursementEligibility() {
-    let reimbursementCosts = { health: 5000, housing: 8000, wellness: 6000, gym: 12000 };
+    let reimbursementCosts = {health: 5000, housing: 8000, wellness: 6000, gym: 12000};
     let totalBenefitsValue = 0;
     let employeeBenefits = payGrades[getCadre()].benefits;
     for (let i = 0; i < employeeBenefits.length; i++) {
@@ -38,4 +43,11 @@ function reimbursementEligibility() {
     return totalBenefitsValue;
 }
 
-export default Employee;
+export {
+    Employee,
+    getCadre as cadre,
+    calculateTax as tax,
+    getBenefits as benefits,
+    calculateBonus as bonus,
+    reimbursementEligibility as reimbursement
+};
