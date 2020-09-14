a953f6b20a9b67090b7da7c1e13c85dea9783692
Paxnamra <bialyfeniks4@gmail.com>
2020-09-14 23:43:40 +0200

    school catalogue: added quickFacts method

--- school-catalogue/main.js
+++ school-catalogue/main.js
@@ -24,4 +24,9 @@ class School {
             console.log('Invalid input: numberOfStudents must be set to a Number.');
         }
     }
+
+    quickFacts() {
+        console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
+    }
+    
 }
