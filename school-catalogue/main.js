09cd2103cd5a64197477dcf75f32c232638181ee
Paxnamra <bialyfeniks4@gmail.com>
2020-09-15 01:00:36 +0200

    school catalogue: added and adjusted constructor inside HighSchool class

--- school-catalogue/main.js
+++ school-catalogue/main.js
@@ -47,4 +47,8 @@ class PrimarySchool extends School {
 }
 
 class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+        super(name, 'high', numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
 }
